b9d6c07fe3389cabc35e5d994ae50bc2e0d64b9b
Ruslan <le0ruslan@mail.ru>
2024-11-21 19:25:38 +0300

    Project is ready

---
+++ src/frontend_calc/mainwindow.h
@@ -0,0 +1,43 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include "creditcalc.h"
+#include "depositcalc.h"
+#include "graph.h"
+
+extern "C" {
+#include "../backend_calc/s21_smartcalc.h"
+}
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+  Q_OBJECT
+
+ public:
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
+
+ private:
+  Ui::MainWindow *ui;
+  Graph *sWindow;
+  CreditCalc *tWindow;
+  DepositCalc *dWindow;
+
+ private slots:
+  void digits_input();
+  void on_pushButtonResetClicked();
+  void on_pushButtonResultClicked();
+  void on_pushButton_delete_clicked();
+  void on_pushButtonGRAPHClicked();
+  void on_pushButton_CREDIT_clicked();
+  void on_pushButton_DEPOSIT_clicked();
+};
+
+#endif  // MAINWINDOW_H
